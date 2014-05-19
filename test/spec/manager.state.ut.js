77236f7581fabe8f1291ac822f12f3ed4ea852ec
Joshua Minzner <josh@minzner.org>
2014-05-19 17:55:48 -0400

    Pull in MiniReels ordered by the date they were last modified.

--- test/spec/manager.state.ut.js
+++ test/spec/manager.state.ut.js
@@ -83,7 +83,7 @@
                 });
 
                 it('should get all the minireels that are associated with the user\'s org', function() {
-                    expect(cinema6.db.findAll).toHaveBeenCalledWith('experience', { type: 'minireel', org: currentUser.org });
+                    expect(cinema6.db.findAll).toHaveBeenCalledWith('experience', { type: 'minireel', org: currentUser.org, sort: 'lastUpdated,-1' });
                 });
 
                 it('should return the model it already has if it has one', function() {
