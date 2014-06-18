af89ab15c6444fd732c3c8560dd6607c07e1b930
Joshua Minzner <josh@minzner.org>
2014-06-18 17:06:40 -0400

    WIP on new c6State API.

--- app/assets/scripts/c6_state.js
+++ app/assets/scripts/c6_state.js
@@ -3,8 +3,73 @@
 
     angular.module('c6.state', [])
         .provider('c6State', [function() {
-            this.$get = function() {
+            var stateConstructors = {
+                Application: (function() {
+                    var Application = function() {};
 
+                    Application.init = function() {
+                        this.cParent = null;
+                        this.cModel = null;
+                        this.cUrl = '/';
+
+                        return this;
+                    };
+
+                    return Application;
+                }())
             };
+
+            function C6State($injector) {
+                var states = {};
+
+                this.get = function(name) {
+                    var constructor = stateConstructors[name];
+
+                    return states[name] ||
+                        (states[name] = constructor.init.call(
+                            $injector.instantiate(constructor),
+                            this
+                        ));
+                };
+            }
+
+            function Mapper(parent) {
+                this.parent = parent;
+            }
+            Mapper.prototype = {
+                state: function(name, mapFn) {
+                    var constructor = stateConstructors[name],
+                        parent = this.parent;
+
+                    constructor.init = function(c6State) {
+                        this.cParent = c6State.get(parent);
+                        this.cUrl = null;
+                        this.cModel = null;
+
+                        return this;
+                    };
+
+                    if (mapFn) {
+                        mapFn.call(new Mapper(name));
+                    }
+                }
+            };
+
+            this.state = function(name, constructor) {
+                stateConstructors[name] = constructor;
+
+                return this;
+            };
+
+            this.map = function(mapFn) {
+                var mapper = new Mapper('Application');
+
+                mapFn.call(mapper);
+            };
+
+            this.$get = ['$injector',
+            function    ( $injector ) {
+                return new C6State($injector);
+            }];
         }]);
 }());
--- test/spec/c6_state.service.ut.js
+++ test/spec/c6_state.service.ut.js
@@ -20,6 +20,97 @@
                 it('should exist', function() {
                     expect(c6StateProvider).toEqual(jasmine.any(Object));
                 });
+
+                describe('@public', function() {
+                    describe('methods', function() {
+                        describe('map(fn)', function() {
+                            beforeEach(function() {
+                                c6StateProvider
+                                    .state('About', [function() {}])
+                                    .state('Posts', [function() {}])
+                                    .state('Posts.Post', [function() {}])
+                                    .state('Posts.Post.Comments', [function() {}]);
+                            });
+
+                            describe('this.state()', function() {
+                                beforeEach(function() {
+                                    c6StateProvider.map(function() {
+                                        this.state('About');
+                                        this.state('Posts', function() {
+                                            this.state('Posts.Post', function() {
+                                                this.state('Posts.Post.Comments');
+                                            });
+                                        });
+                                    });
+                                });
+
+                                it('should set up relationships between states', function() {
+                                    var about = c6State.get('About'),
+                                        posts = c6State.get('Posts'),
+                                        postsPost = c6State.get('Posts.Post'),
+                                        postsPostComments = c6State.get('Posts.Post.Comments'),
+                                        application = c6State.get('Application');
+
+                                    expect(about.cParent).toBe(application);
+                                    expect(posts.cParent).toBe(application);
+                                    expect(postsPost.cParent).toBe(posts);
+                                    expect(postsPostComments.cParent).toBe(postsPost);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+
+            describe('service', function() {
+                it('should exist', function() {
+                    expect(c6State).toEqual(jasmine.any(Object));
+                });
+
+                describe('@public', function() {
+                    describe('methods', function() {
+                        describe('get(state)', function() {
+                            var Home,
+                                home;
+
+                            beforeEach(function() {
+                                Home = [function() {}];
+
+                                c6StateProvider.state('Home', Home);
+
+                                c6StateProvider.map(function() {
+                                    this.state('Home');
+                                });
+
+                                home = c6State.get('Home');
+                            });
+
+                            it('should get the auto-generated Application state', function() {
+                                var application = c6State.get('Application');
+
+                                expect(application.cModel).toBeNull();
+                                expect(application.cParent).toBeNull();
+                                expect(application.cUrl).toBe('/');
+                            });
+
+                            it('should be an instance of the provided constructor', function() {
+                                expect(home).toEqual(jasmine.any(Home[0]));
+                            });
+
+                            it('should be decorated with properties', function() {
+                                expect(home.cModel).toBeNull();
+                                expect(home.cParent).toBe(c6State.get('Application'));
+                                expect(home.cUrl).toBeNull();
+                            });
+
+                            it('should return the same instance', function() {
+                                var home2 = c6State.get('Home');
+
+                                expect(home).toBe(home2);
+                            });
+                        });
+                    });
+                });
             });
         });
     });
