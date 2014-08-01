b603754b66843c5c72da6ad2c732354f34de372c
Joshua Minzner <josh@minzner.org>
2014-08-01 13:50:29 -0400

    Added <c6-embed> directive for embedding MiniReels via the actual embed script.

--- app/assets/scripts/preview_minireel.js
+++ app/assets/scripts/preview_minireel.js
@@ -19,5 +19,56 @@ function( angular , c6State  ) {
             }]);
         }])
 
-        .controller('PreviewMiniReelController', [function() {}]);
+        .controller('PreviewMiniReelController', [function() {}])
+
+        .directive('c6Embed', ['$window','$compile',
+        function              ( $window , $compile ) {
+            var pending = $window.c6.pending || ($window.c6.pending = []);
+
+            function link(scope, $element) {
+                scope.$watchCollection('config', function(config) {
+                    var id = Math.random().toString(34).slice(2);
+
+                    $element.empty();
+
+                    if (!config) { return; }
+
+                    pending.push(id);
+
+                    $compile([
+                        '<script ',
+                        Object.keys(config)
+                            .map(function(attr) {
+                                return [attr, config[attr]]
+                                    .filter(function(item, index) {
+                                        return index === 0 || item !== true;
+                                    })
+                                    .map(function(item, index) {
+                                        return index === 0 ?
+                                            ('data-' + item) : ('"' + item + '"');
+                                    });
+                            })
+                            .concat([
+                                ['id', id],
+                                ['src', scope.src]
+                            ])
+                            .map(function(pairs) {
+                                return pairs.join('=');
+                            })
+                            .join(' '),
+                        '></script>'
+                    ].join(''))(scope)
+                        .appendTo($element);
+                });
+            }
+
+            return {
+                restrict: 'E',
+                link: link,
+                scope: {
+                    config: '=',
+                    src: '@'
+                }
+            };
+        }]);
 });
---
+++ test/spec/c6_embed.directive.ut.js
@@ -0,0 +1,81 @@
+define(['preview_minireel'], function(previewMiniReelModule) {
+    'use strict';
+
+    describe('<c6-embed>', function() {
+        var $rootScope,
+            $scope,
+            $compile;
+
+        var $window;
+
+        var $c6Embed;
+
+        beforeEach(function() {
+            module('ng', function($provide) {
+                $provide.value('$window', {
+                    c6: {}
+                });
+            });
+            module(previewMiniReelModule.name);
+
+            inject(function($injector) {
+                $rootScope = $injector.get('$rootScope');
+                $compile = $injector.get('$compile');
+
+                $window = $injector.get('$window');
+            });
+
+            $scope = $rootScope.$new();
+
+            $scope.config = null;
+
+            $scope.$apply(function() {
+                $c6Embed = $compile('<c6-embed src="//lib.cinema6.com/c6embed/v1/c6embed.min.js" config="config"></c6-embed>')($scope);
+            });
+        });
+
+        describe('when a config is provided', function() {
+            beforeEach(function() {
+                $scope.$apply(function() {
+                    $scope.config = {
+                        exp: 'e-e7de1bfac3e1af',
+                        preload: true,
+                        splash: 'horizontal-stack:16/9',
+                        title: 'Awesome MiniReel!',
+                        branding: 'digitaljournal'
+                    };
+                });
+            });
+
+            it('should add an id to the c6.pending array', function() {
+                expect($window.c6.pending).toEqual([jasmine.any(String)]);
+            });
+
+            it('should put a c6Embed script in the DOM', function() {
+                var id = $window.c6.pending[0],
+                    $script = $c6Embed.children('script');
+
+                expect($script.length).toBe(1);
+
+                expect($script.attr('id')).toBe(id);
+                expect($script.attr('src')).toBe('//lib.cinema6.com/c6embed/v1/c6embed.min.js');
+                expect($script.attr('data-exp')).toBe($scope.config.exp);
+                expect($script.attr('data-preload')).toBe('');
+                expect($script.attr('data-splash')).toBe($scope.config.splash);
+                expect($script.attr('data-title')).toBe($scope.config.title);
+                expect($script.attr('data-branding')).toBe($scope.config.branding);
+            });
+
+            it('should clear out the element whenever the config changes', function() {
+                $c6Embed.append('<div>');
+
+                $scope.$apply(function() {
+                    $scope.config.preload = false;
+                });
+
+                expect($c6Embed.children('script').length).toBe(1);
+                expect($c6Embed.children().length).toBe(1);
+            });
+        });
+    });
+});
