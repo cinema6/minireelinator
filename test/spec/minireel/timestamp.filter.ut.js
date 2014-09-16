cc8ac4f0265254f61ed98e3ef51e444310a82af4
Joshua Minzner <josh@minzner.org>
2014-09-16 14:50:25 -0400

    Added timestampFilter() for converting seconds into a human-readable timestamp.

---
+++ test/spec/minireel/timestamp.filter.ut.js
@@ -0,0 +1,28 @@
+define(['minireel/app'], function(appModule) {
+    'use strict';
+
+    describe('timestampFilter()', function() {
+        var timestampFilter;
+
+        beforeEach(function() {
+            module(appModule.name);
+
+            inject(function($injector) {
+                timestampFilter = $injector.get('timestampFilter');
+            });
+        });
+
+        it('should exist', function() {
+            expect(timestampFilter).toEqual(jasmine.any(Function));
+        });
+
+        it('should convert seconds to a human-readable timestamp', function() {
+            expect(timestampFilter(60)).toBe('1:00');
+            expect(timestampFilter(65)).toBe('1:05');
+            expect(timestampFilter(70)).toBe('1:10');
+            expect(timestampFilter(125)).toBe('2:05');
+            expect(timestampFilter(31)).toBe('0:31');
+            expect(timestampFilter(3725)).toBe('1:02:05');
+        });
+    });
+});
