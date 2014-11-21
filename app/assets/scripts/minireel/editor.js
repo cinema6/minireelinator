ec380880d264534459b1db3ef0b2bdbe2936bca7
Scott Munson <scott@thinkcontra.com>
2014-11-21 16:45:35 -0500

    update primary button

--- app/assets/scripts/minireel/editor.js
+++ app/assets/scripts/minireel/editor.js
@@ -1304,18 +1304,18 @@ VideoCardController           ) {
                     get: function() {
                         var state = c6State.current;
 
-                        if (/^(MR:EditCard.(Video|Ballot))$/.test(state)) {
+                        if (/^(MR:EditCard.(Copy|Ballot))$/.test(state)) {
                             return copy({
                                 text: EditorCtrl.model.status === 'active' ? 'I\'m Done!' : 'Save',
                                 action: function() { self.save(); },
-                                enabled: this.canSave
+                                enabled: this.videoComplete && this.canSave
                             }, primaryButton);
                         }
 
                         return copy({
                             text: 'Next Step',
-                            action: function() { c6State.goTo('MR:EditCard.Video'); },
-                            enabled: this.copyComplete && !EditorCtrl.errorForCard(this.model)
+                            action: function() { c6State.goTo('MR:EditCard.Copy'); },
+                            enabled: this.videoComplete && !EditorCtrl.errorForCard(this.model)
                         }, primaryButton);
                     }
                 },
