e3fafb6fbece5e582f3a70b33fe7e83685c3dfd2
Scott Munson <scott@thinkcontra.com>
2015-07-09 19:13:48 -0400

    add SelfieVideoService

--- app/assets/scripts/selfie.js
+++ app/assets/scripts/selfie.js
@@ -1,6 +1,10 @@
-define( ['angular','c6_state','c6uilib'],
+define( ['angular','c6_state','c6uilib','select2'],
 function( angular , c6State  , c6uilib ) {
     'use strict';
+
+    var extend = angular.extend,
+        fromJson = angular.fromJson,
+        toJson = angular.toJson;
 
     return angular.module('c6.app.selfie', [c6State.name, c6uilib.name])
         .config(['c6StateProvider',
@@ -101,43 +105,172 @@ function( angular , c6State  , c6uilib ) {
             };
         }])
 
-        .directive('c6FillCheck', [function() {
+        .directive('c6FillCheck', ['$timeout',
+        function                  ( $timeout ) {
             return {
                 restrict: 'A',
                 link: function(scope, $element) {
-                    scope.$watch(function() {
-                        return $element.val();
-                    }, function(newValue, oldVal) {
-                        if (newValue !== oldVal) { return; }
-
-                        if (newValue) {
+                    $timeout(function() {
+                        if ($element.val()) {
                             $element.addClass('form__fillCheck--filled');
-                        } else {
-                            $element.removeClass('form__fillCheck--filled');
                         }
+
+                        $element.blur(function() {
+                            if ($element.val()){
+                                $element.addClass('form__fillCheck--filled');
+                            } else {
+                                $element.removeClass('form__fillCheck--filled');
+                            }
+                        });
                     });
                 }
             };
         }])
 
-        .directive('c6SelectBox', [function() {
+        .directive('c6SelectBox', ['$timeout',
+        function                  ( $timeout ) {
             return {
                 restrict: 'A',
-                link: function(scope, $element) {
-                    $element.select2({
-                        minimumResultsForSearch: Infinity
-                    });
+                link: function(scope, $element, attrs) {
+                    $timeout(function() {
+                        $element.select2({
+                            minimumResultsForSearch: Infinity
+                        });
 
-                    $element.on('select2:open', function() {
-                        $($element).addClass('form__fillCheck--filled');
-                        $($element).addClass('ui--active');
-                        //$('.form__selectBox--single ~ .form__helpBox' ).addClass('form__helpBox--show');
-                    });
+                        if (attrs.preselected) {
+                            $element.addClass('form__fillCheck--filled');
+                        }
 
-                    $element.on('select2:close', function() {
-                        $($element).removeClass('ui--active');
-                        //$('.form__selectBox--single ~ .form__helpBox' ).removeClass('form__helpBox--show');
+                        $element.on('select2:open', function() {
+                            $element.addClass('form__fillCheck--filled');
+                            $element.addClass('ui--active');
+                        });
+
+                        $element.on('select2:close', function() {
+                            $element.removeClass('ui--active');
+                        });
                     });
+                }
+            };
+        }])
+
+        .service('SelfieVideoService', ['$http','c6UrlParser','$q',
+        function                       ( $http , c6UrlParser , $q ) {
+            function getJSONProp(json, prop) {
+                return (fromJson(json) || {})[prop];
+            }
+
+            function setJSONProp(json, prop, value) {
+                var proto = {};
+
+                proto[prop] = value;
+
+                return toJson(extend(fromJson(json) || {}, proto));
+            }
+
+            function validateVast(url) {
+                return $http.get(url)
+                    .then(function(response) {
+                        var isVast = /VAST/.test(response.data),
+                            isXml = response.headers()['content-type'] === 'text/xml';
+
+                        if (!isVast || !isXml) {
+                            return $q.reject('Not a valid VAST tag');
+                        }
+
+                        return {
+                            service: 'adUnit',
+                            id: setJSONProp(null, 'vast', url)
+                        };
+                    });
+            }
+
+            this.dataFromUrl = function(url) {
+                var parsed = c6UrlParser(url),
+                    service = (parsed.hostname.match(
+                        /youtube|dailymotion|vimeo|aol|yahoo|rumble/
+                    ) || [])[0],
+                    id,
+                    idFetchers = {
+                        youtube: function(url) {
+                            return params(url.search).v;
+                        },
+                        vimeo: function(url) {
+                            return url.pathname.replace(/^\//, '');
+                        },
+                        dailymotion: function(url) {
+                            var pathname = url.pathname;
+
+                            if (pathname.search(/^\/video\//) < 0) {
+                                return null;
+                            }
+
+                            return (pathname
+                                .replace(/\/video\//, '')
+                                .match(/[a-zA-Z0-9]+/) || [])[0];
+                        },
+                        aol: function(url) {
+                            return (url.pathname.match(/[^\/]+$/) || [null])[0];
+                        },
+                        yahoo: function(url) {
+                            return (url.pathname
+                                .match(/[^/]+(?=(\.html))/) || [null])[0];
+                        },
+                        rumble: function(url) {
+                            return (url.pathname
+                                .match(/[^/]+(?=(\.html))/) || [null])[0];
+                        }
+                    };
+
+                function params(search) {
+                    return search.split('&')
+                        .map(function(pair) {
+                            return pair.split('=')
+                                .map(decodeURIComponent);
+                        })
+                        .reduce(function(params, pair) {
+                            params[pair[0]] = pair[1];
+
+                            return params;
+                        }, {});
+                }
+
+                if (!service) {
+                    if (/^http|https|\/\//.test(url)) {
+                        return validateVast(url);
+                    } else {
+                        return $q.reject('Unable to determine service');
+                    }
+                }
+
+                id = idFetchers[service](parsed);
+
+                if (!id) { return $q.reject('Unable to find id'); }
+
+                return $q.when({
+                    service: service,
+                    id: id
+                });
+            };
+
+            this.urlFromData = function(service, id) {
+                switch (service) {
+
+                case 'youtube':
+                    return 'https://www.youtube.com/watch?v=' + id;
+                case 'vimeo':
+                    return 'http://vimeo.com/' + id;
+                case 'dailymotion':
+                    return 'http://www.dailymotion.com/video/' + id;
+                case 'aol':
+                    return 'http://on.aol.com/video/' + id;
+                case 'yahoo':
+                    return 'https://screen.yahoo.com/' + id + '.html';
+                case 'rumble':
+                    return 'https://rumble.com/' + id + '.html';
+                case 'adUnit':
+                    return getJSONProp(id, 'vast');
+
                 }
             };
         }]);
