5b1bc538e184f536657588a60f610a384935f390
Joshua Minzner <josh@minzner.org>
2015-11-10 12:21:55 -0500

    Use hostname to determine if selfie or not.

--- app/assets/scripts/c6_defines.js
+++ app/assets/scripts/c6_defines.js
@@ -2,10 +2,12 @@ define(function() {
     'use strict';
 
     var c6 = window.c6 = {};
+    var hostname = window.location.hostname;
+    var protocol = window.location.protocol;
 
     c6.kLocal = !!window.DEBUG;
-    c6.kSelfie = !!window.SELFIE;
-    c6.kDebug = c6.kLocal || window.location.hostname === 'staging.cinema6.com';
+    c6.kSelfie = !!window.SELFIE || (/platform/).test(hostname);
+    c6.kDebug = c6.kLocal || (/staging/).test(hostname);
     c6.kHasKarma = false;
     c6.kLogFormats = c6.kDebug;
     c6.kLogLevels = c6.kDebug ? ['error','warn','log','info'] : [];
@@ -21,7 +23,7 @@ define(function() {
 
     if (c6.kDebug) {
         // This URL must include the page's protocol to work in Firefox
-        window.__C6_URL_ROOT__ = window.location.protocol + '//staging.cinema6.com';
+        window.__C6_URL_ROOT__ = protocol + '//' + (c6.kLocal ? 'staging.cinema6.com' : hostname);
     }
 
     return c6;
