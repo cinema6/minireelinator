5f66e3df23d23adc329d8f097a54f739e9186c32
djani <janidhaval@gmail.com>
2015-11-24 12:34:59 -0500

    Reverted the alert message
    Reverted the alert message

--- app/assets/scripts/selfie/campaign.js
+++ app/assets/scripts/selfie/campaign.js
@@ -407,8 +407,7 @@ function( angular , c6State  , PaginatedListState                    ,
                             return $q.when(null);
                         } else {
                             ConfirmDialogService.display({
-                                prompt: 'Are you sure you want to leave? You will ' +
-                                    'lose your changes if you continue.',
+                                prompt: 'Are you sure you want to lose your changes?',
                                 affirm: 'Yes',
                                 cancel: 'No',
 
