284d320ad178af9db11ba9db10e5573578c9383f
Scott Munson <sqmunson@gmail.com>
2015-12-29 12:04:56 -0500

    remove high density test in AppCtrl

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1268,8 +1268,8 @@ function( angular , ngAnimate , minireel     , account     , login , portal , c6
             });
         }])
 
-        .controller('AppController', ['CSSLoadingService','cState','$window',
-        function                     ( CSSLoadingService , cState , $window ) {
+        .controller('AppController', ['CSSLoadingService','cState',
+        function                     ( CSSLoadingService , cState ) {
             var appStyles = {
                 Portal: [
                     'styles/c6main.css',
@@ -1287,23 +1287,10 @@ function( angular , ngAnimate , minireel     , account     , login , portal , c6
                 ]
             };
 
-            function isHighDensity(){
-                return (($window.matchMedia &&
-                    ($window.matchMedia('only screen and (min-resolution: 124dpi), '+
-                        'only screen and (min-resolution: 1.3dppx), '+
-                        'only screen and (min-resolution: 48.8dpcm)').matches ||
-                    $window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), '+
-                        'only screen and (-o-min-device-pixel-ratio: 2.6/2), '+
-                        'only screen and (min--moz-device-pixel-ratio: 1.3), '+
-                        'only screen and (min-device-pixel-ratio: 1.3)').matches)) ||
-                    ($window.devicePixelRatio && $window.devicePixelRatio > 1.3));
-            }
-
             CSSLoadingService.load(appStyles[cState.name]);
 
             this.version = version;
             this.validImgSrc = /^(http:\/\/|https:\/\/|\/\/)/;
             this.validUrl = /^(http:\/\/|https:\/\/|\/\/)/;
-            this.isHighDensity = isHighDensity();
         }]);
 });
